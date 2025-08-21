dfed64139fd51958a69d0cd29dd9bd2033cec767
ocokura <ocokura@gmail.com>
2025-08-22 03:51:24 +0900

    first commit

--- mobhealth-template-1.20.1/src/main/java/net/ocoserver/ClientMain.java
+++ mobhealth-template-1.20.1/src/main/java/net/ocoserver/ClientMain.java
@@ -5,7 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class MobHealth implements ModInitializer {
+public class ClientMain implements ModInitializer {
 	public static final String MOD_ID = "mobhealth";
 
 	// This logger is used to write text to the console and the log file.
